069101b3aad8018a3fea0a86040fec961a38bb6a
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-04-18 10:47:34 -0500

    Added AddScoped method to Hosting.

--- BlazorWebAssembly/CarvedRock-BlazorWebAssembly/CarvedRock-BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/CarvedRock-BlazorWebAssembly/CarvedRock-BlazorWebAssembly/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+//builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("https://localhost:7273") });
+builder.Services.AddScoped<IProductApiService, ProductApiService>;
 
 await builder.Build().RunAsync();
